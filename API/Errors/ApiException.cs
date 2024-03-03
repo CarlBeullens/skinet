391a7934e9e4293a7b8642fc975709e19242ed25
Carl Beullens <carl.beullens@rsvz-inasti.fgov.be>
2024-03-03 20:38:12 +0100

    End of section 5

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,13 @@
+﻿using API.Errors;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class ApiException : ApiResponse
+{
+	public ApiException(int statusCode, string message = null, string details = null) : base(statusCode, message)
+	{
+		Details = details;
+	}
+
+	public string Details { get; set; }
+}
